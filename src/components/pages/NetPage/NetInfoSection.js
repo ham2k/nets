a2bebd0b5e599d8bebe0867624399ea7cab1a305
Sebastian Delmont <sd@notso.net>
2021-06-08 09:36:00 -0400

    Net info form [wip]

--- src/components/pages/NetPage/NetInfoSection.js
+++ src/components/pages/NetPage/NetInfoSection.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classNames from 'classnames'
 
 import {
   Accordion,
+  AccordionActions,
   AccordionDetails,
   AccordionSummary,
+  Button,
   Grid,
   InputLabel,
   makeStyles,
@@ -17,8 +19,12 @@ import InfoIcon from '@material-ui/icons/Info'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 import CheckinsLoader from '../../checkins/CheckinsLoader'
+import BANDS from '../../../data/consts/bands'
+import MODES from '../../../data/consts/modes'
 
 import baseStyles from './styles'
+import { useSelector } from 'react-redux'
+import { serversSelector } from '../../../data/netlogger'
 
 const useStyles = makeStyles((theme) => ({
   ...baseStyles(theme),
@@ -45,61 +51,39 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-const NET_MODES = [
-  'SSB',
-  'AM',
-  'FM',
-  'CW',
-  'RTTY',
-  'PSK31',
-  'PSK63',
-  'JT9',
-  'JT65',
-  'PACKET',
-  'AMTOR',
-  'PACTOR',
-  'SSTV',
-  'MT63',
-  'OLIVIA',
-  'Other',
-]
-const NET_BANDS = [
-  '2190m',
-  '560m',
-  '160m',
-  '80m',
-  '60m',
-  '40m',
-  '30m',
-  '20m',
-  '17m',
-  '15m',
-  '12m',
-  '10m',
-  '6m',
-  '4m',
-  '2m',
-  '1.25m',
-  '70cm',
-  '33cm',
-  '23cm',
-  '13cm',
-  '9cm',
-  '6cm',
-  '3cm',
-  '1.25cm',
-  '70cm',
-  '6mm',
-  '4mm',
-  '2.5mm',
-  '2mm',
-  '1mm',
-]
-
+const defaultFormValues = (net) => {
+  const { NetName, Frequency, Band, Mode, NetControl, ServerName, Password } = net
+  return { modified: false, NetName, Frequency, Band, Mode, NetControl, ServerName, Password }
+}
 export default function NetInfoSection({ net, className, style, onViewChange, currentView }) {
   const classes = useStyles()
-  const editable = true
-  const readOnly = !editable
+
+  const servers = useSelector(serversSelector())
+
+  const [form, setFormData] = useState(() => defaultFormValues(net))
+
+  const handleForm = (ev) => {
+    const field = ev.target?.dataset?.field
+    const value = ev.target?.value
+    if (field) {
+      setFormData({
+        ...form,
+        [field]: value,
+        modified: form.modified || net[field] !== value,
+      })
+    } else {
+      console.log(ev.target)
+    }
+  }
+
+  const handleCancel = () => {
+    setFormData(defaultFormValues(net))
+  }
+
+  const handleSave = () => {}
+
+  const isNew = false
+  const isEditable = form.Password
 
   return (
     <Accordion className={classNames(className, classes.sectionRoot)} style={style} square>
@@ -118,17 +102,37 @@ export default function NetInfoSection({ net, className, style, onViewChange, cu
       <AccordionDetails className={classes.sectionIndented}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <TextField label="Net Name" value={net.NetName || ''} disabled={readOnly} fullWidth={true} />
+            <TextField
+              label="Net Name"
+              value={form.NetName || ''}
+              onChange={handleForm}
+              inputProps={{ 'data-field': 'NetName' }}
+              disabled={!isEditable}
+              fullWidth={true}
+            />
           </Grid>
           <Grid item xs={4}>
-            <TextField label="Frequency" value={net.Frequency || ''} disabled={readOnly} fullWidth={true} />
+            <TextField
+              label="Frequency"
+              value={form.Frequency || ''}
+              onChange={handleForm}
+              inputProps={{ 'data-field': 'Frequency' }}
+              disabled={!isEditable}
+              fullWidth={true}
+            />
           </Grid>
           <Grid item xs={4}>
-            <InputLabel id="net-mode-label" disabled={readOnly}>
+            <InputLabel id="net-mode-label" disabled={!isEditable}>
               Mode
             </InputLabel>
-            <Select labelId="net-mode-label" disabled={readOnly} value={net.Mode || ''} fullWidth={true}>
-              {NET_MODES.map((mode) => (
+            <Select
+              labelId="net-mode-label"
+              disabled={!isEditable}
+              value={form.Mode || ''}
+              onChange={(ev) => handleForm({ target: { value: ev.target.value, dataset: { field: 'Mode' } } })}
+              fullWidth={true}
+            >
+              {MODES.map((mode) => (
                 <MenuItem key={mode} value={mode}>
                   {mode}
                 </MenuItem>
@@ -136,11 +140,17 @@ export default function NetInfoSection({ net, className, style, onViewChange, cu
             </Select>{' '}
           </Grid>
           <Grid item xs={4}>
-            <InputLabel id="net-band-label" disabled={readOnly}>
+            <InputLabel id="net-band-label" disabled={!isEditable}>
               Band
             </InputLabel>
-            <Select labelId="net-band-label" disabled={readOnly} value={net.Band || ''} fullWidth={true}>
-              {NET_BANDS.map((band) => (
+            <Select
+              labelId="net-band-label"
+              disabled={!isEditable}
+              value={form.Band || ''}
+              onChange={(ev) => handleForm({ target: { value: ev.target.value, dataset: { field: 'Band' } } })}
+              fullWidth={true}
+            >
+              {BANDS.map((band) => (
                 <MenuItem key={band} value={band}>
                   {band}
                 </MenuItem>
@@ -148,15 +158,41 @@ export default function NetInfoSection({ net, className, style, onViewChange, cu
             </Select>
           </Grid>
           <Grid item xs={4} sm={3}>
-            <TextField label="Net Control" value={net.NetControl || ''} disabled={readOnly} fullWidth={true} />
+            <TextField
+              label="Net Control"
+              value={form.NetControl || ''}
+              disabled={!isEditable}
+              onChange={handleForm}
+              inputProps={{ 'data-field': 'NetControl' }}
+              fullWidth={true}
+            />
           </Grid>
+
           <Grid item xs={8} sm={3}>
-            <TextField label="Server" value={net.ServerName || ''} disabled={readOnly} fullWidth={true} />
+            <InputLabel id="net-server-label" disabled={!isEditable || !isNew}>
+              Server
+            </InputLabel>
+
+            <Select
+              labelId="net-band-label"
+              disabled={!isEditable || !isNew}
+              value={form.ServerName || ''}
+              onChange={(ev) => handleForm({ target: { value: ev.target.value, dataset: { field: 'ServerName' } } })}
+              fullWidth={true}
+            >
+              {servers.map((server) => (
+                <MenuItem key={server.ServerName} value={server.ServerName}>
+                  {server.ServerName}
+                </MenuItem>
+              ))}
+            </Select>
           </Grid>
           <Grid item xs={12} sm={6}>
             <TextField
               label="Net Password"
-              value={net.Password || ''}
+              value={form.Password || ''}
+              onChange={handleForm}
+              inputProps={{ 'data-field': 'Password' }}
               placeholder={'Enter the net password if you know it'}
               type={'password'}
               fullWidth={true}
@@ -167,6 +203,14 @@ export default function NetInfoSection({ net, className, style, onViewChange, cu
           </Grid>
         </Grid>
       </AccordionDetails>
+      <AccordionActions>
+        <Button size="small" onClick={handleCancel} disabled={!form.modified}>
+          Cancel
+        </Button>
+        <Button size="small" onClick={handleSave} color="primary" disabled={!form.modified}>
+          Save
+        </Button>
+      </AccordionActions>
     </Accordion>
   )
 }
---
+++ src/data/consts/modes.js
@@ -0,0 +1,20 @@
+export const MODES = [
+  'SSB',
+  'AM',
+  'FM',
+  'CW',
+  'RTTY',
+  'PSK31',
+  'PSK63',
+  'JT9',
+  'JT65',
+  'PACKET',
+  'AMTOR',
+  'PACTOR',
+  'SSTV',
+  'MT63',
+  'OLIVIA',
+  'Other',
+]
+
+export default MODES
