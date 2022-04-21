e74705de5c12d16187b170ccc3db661129037005
Sebastian Delmont <sd@notso.net>
2022-04-21 09:06:40 -0400

    Use SplitPane from https://github.com/JeremyGrieshop/react-split-pane

---
+++ src/utils/split-pane/index.js
@@ -0,0 +1,9 @@
+// This was taken from:  https://github.com/tomkp/react-split-pane
+// It was an MIT-licensed project, which never got updated and caused all sorts of 
+// package dependency issues because of this.
+
+import ReactSplitPane from "./SplitPane";
+import Pane from "./Pane";
+
+export default ReactSplitPane
+export { Pane };
